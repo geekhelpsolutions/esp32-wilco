7e61c202b0c0abf9b06104cdd5074afab44c2ee2
Grant Le Roux <grant@geekhelp.com.au>
2018-11-25 23:33:50 +1100

    Initial commit

---
+++ src/esp32_wilco.h
@@ -0,0 +1,19 @@
+/*
+ Name:		esp32_wilco.h
+ Created:	11/25/2018 11:31:18 PM
+ Author:	grant
+ Editor:	http://www.visualmicro.com
+*/
+
+#ifndef _esp32_wilco_h
+#define _esp32_wilco_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
