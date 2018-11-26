ceff320b3b8f0309a4e88dbd44c5f1403b1f4ad5
Grant Le Roux <grant@geekhelp.com.au>
2018-11-26 23:17:25 +1100

    Add MQTT

--- src/WILCO.cpp
+++ src/WILCO.cpp
@@ -4,9 +4,10 @@
 
 #include "WILCO.h"
 #include <WiFi.h>
+#include <WiFiClientSecure.h>
 #include <DNSServer.h>
 #include <WebServer.h>
-
+#include <PubSubClient.h>
 
 #define DNS_PORT  53
 #define DNS_TTL   300
@@ -16,16 +17,22 @@
 
 DNSServer dnsServer;
 WebServer webServer(HTTP_PORT);
-
+WiFiClientSecure wifiClient;
+PubSubClient mqttClient(wifiClient);
 
 void WILCO::begin() {
 	_namespace.begin();
-
+	if ((!_settingWiFiSSID.exists()) || (!_settingWiFiPassword.exists())) {
+		_startAPMode();
+		return;
+	}
+	_startClientMode();
 }
 
 void WILCO::loop() {
 	dnsServer.processNextRequest();
 	webServer.handleClient();
+	mqttClient.loop();
 }
 
 void WILCO::_startAPMode() {
@@ -37,6 +44,10 @@ void WILCO::_startAPMode() {
 	}
 	Serial.println(" done.");
 
+	_ip = WiFi.softAPIP();
+	Serial.print("IP: ");
+	Serial.println(_ip);
+
 	_startDNSServer();
 	_startWebServer();
 }
@@ -44,6 +55,10 @@ void WILCO::_startAPMode() {
 void WILCO::_startClientMode() {
 	Serial.print("Starting Client Mode...");
 	WiFi.mode(WIFI_MODE_STA);
+	WiFi.begin(
+		_settingWiFiSSID.load().c_str(),
+		_settingWiFiPassword.load().c_str()
+	);
 	long timeout = millis() + (AP_TIMEOUT * 1000);
 	while (!WiFi.isConnected()) {
 		if (millis() >= timeout) {
@@ -54,14 +69,18 @@ void WILCO::_startClientMode() {
 	}
 	Serial.println(" done.");
 
+	_ip = WiFi.localIP();
+	Serial.print("IP: ");
+	Serial.println(_ip);
+
 	_startWebServer();
+	_startMQTTClient();
 }
 
 void WILCO::_startDNSServer() {
 	Serial.print("Starting DNS Server...");
-	IPAddress ip = WiFi.localIP();
 	dnsServer.setTTL(DNS_TTL);
-	if (!dnsServer.start(DNS_PORT, "*", ip)) {
+	if (!dnsServer.start(DNS_PORT, "*", _ip)) {
 		Serial.println(" failed.");
 		return;
 	}
@@ -69,9 +88,75 @@ void WILCO::_startDNSServer() {
 }
 
 void WILCO::_startWebServer() {
+
+	Serial.print("Starting Web Server...");
+
+	webServer.onNotFound([this]() {
+		if (webServer.method() == HTTP_GET) _serveSettingsForm();
+		if (webServer.method() == HTTP_POST) _handleSettingsForm();
+	});
+	webServer.begin();
+
+	Serial.println(" done.");
+
+}
+
+void WILCO::_startMQTTClient() {
+	Serial.print("Connecting MQTT Client...");
+
+	mqttClient.setCallback([this](char * topic, byte * payload, unsigned int length) {
+		char * data = (char*)payload;
+		data[length] = '\0';
+
+		String topicString(topic);
+		String payloadString(data);
+
+		Serial.printf("Topic: %s\n", topicString.c_str());
+		Serial.printf("Data:\n%s\n", payloadString.c_str());
+	});
+
+	String serverString = _settingMQTTServer.load();
+	char * server = new char[serverString.length() + 1];
+	for (int i = 0; i < serverString.length(); i++) {
+		server[i] = serverString.charAt(i);
+	}
+	server[serverString.length()] = '\0';
+
+	mqttClient.setServer(
+		server,
+		_settingMQTTPort.load()
+	);
+	if (!mqttClient.connect(
+		_mqttId().c_str(),
+		_settingMQTTUsername.load().c_str(),
+		_settingMQTTPassword.load().c_str()
+	)) {
+		Serial.print(" failed (");
+		Serial.print(mqttClient.state());
+		Serial.println(").");
+		return;
+	}
+
+	Serial.println(" done.");
+	mqttClient.subscribe("test");
+
+}
+
+String WILCO::_mqttId() {
+	String mac = WiFi.macAddress();
+	mac.replace(":", "");
+	return "ESP_" + mac.substring(6);
+}
+
+void WILCO::_serveSettingsForm() {
+
 	String fields = "";
 	fields += _generateFormField(&_settingWiFiSSID);
 	fields += _generateFormField(&_settingWiFiPassword, true);
+	fields += _generateFormField(&_settingMQTTServer);
+	fields += _generateFormField(&_settingMQTTPort);
+	fields += _generateFormField(&_settingMQTTUsername);
+	fields += _generateFormField(&_settingMQTTPassword, true);
 
 	String html =
 		"<!DOCTYPE html>\n"
@@ -99,23 +184,67 @@ void WILCO::_startWebServer() {
 		"    <form method=\"post\">\n"
 		"      <h1>WILCO Settings</h1>"
 		+ fields +
+		"    <input type=\"submit\" value=\"Save\" />"
 		"    </form>\n"
 		"  </body>\n"
 		"</html>";
 
+	webServer.send(200, "text/html", html);
+
 }
 
-String WILCO::_generateFormField(NVSStringEntry *setting, bool isPassword = false) {
+void WILCO::_handleSettingsForm() {
+	for (int argNum = 0; argNum < webServer.args(); argNum++) {
+		String key = webServer.argName(argNum);
+		String value = webServer.arg(argNum);
+
+		if (key == NVS_KEY_WIFISSID) _settingWiFiSSID.save(value);
+		if (key == NVS_KEY_WIFIPASSWORD) _settingWiFiPassword.save(value);
+		if (key == NVS_KEY_MQTTSERVER) _settingMQTTServer.save(value);
+		if (key == NVS_KEY_MQTTPORT) _settingMQTTPort.save(value.toInt());
+		if (key == NVS_KEY_MQTTUSERNAME) _settingMQTTUsername.save(value);
+		if (key == NVS_KEY_MQTTPASSWORD) _settingMQTTPassword.save(value);
+	}
+
+	String html =
+		"<!DOCTYPE html>\n"
+		"<html>\n"
+		"  <head>\n"
+		"    <meta charset=\"utf-8\">\n"
+		"    <meta name=\"viewport\" content=\"width=device-width, initial-scale:1.0\">\n"
+		"    <title>WILCO Settings</title>\n"
+		"  </head>\n"
+		"  <body>\n"
+		"    <h2>Saved. Rebooting.</h2>\n"
+		"  </body>\n"
+		"</html>";
+
+	webServer.send(200, "text/html", html);
+	delay(1000);
+	esp_restart();
+}
+
+String WILCO::_generateFormField(NVSStringEntry *setting, bool isPassword) {
 	String type = isPassword ? "password" : "text";
-	return String(
-		"      <label for=\"" + setting->name() + "\">" + setting->name() + "</label>"
-		"      <input type=\"" + type + "\" id=\"" + setting->name() + "\" name=\"" + setting->name() + "\" value=\"" + setting->load() + "\" />"
-	);
+	String name = setting->name();
+	String label = setting->label();
+	String value = setting->exists() ? setting->load() : "";
+
+	String output = "";
+	output += (String)"<label for=\"" + name + "\">" + label + "</label>\n";
+	output += (String)"<input type=\"" + type + "\" id=\"" + name + "\" name=\"" + name + "\" value=\"" + value + "\" />\n";
+	
+	return output;
 }
 
 String WILCO::_generateFormField(NVSIntegerEntry *setting) {
-	return String(
-		"      <label for=\"" + setting->name() + "\">" + setting->name() + "</label>"
-		"      <input type=\"text\" id=\"" + setting->name() + "\" name=\"" + setting->name() + "\" value=\"" + setting->load() + "\" />"
-	);
+	String name = setting->name();
+	String label = setting->label();
+	int value = setting->exists() ? setting->load() : NULL;
+	
+	String output = "";
+	output += (String)"<label for=\"" + name + "\">" + label + "</label>\n";
+	output += (String)"<input type=\"number\" id=\"" + name + "\" name=\"" + name + "\" value=\"" + value + "\" />\n";
+
+	return output;
 }
--- src/WILCO.h
+++ src/WILCO.h
@@ -8,13 +8,27 @@
 #include "NVSStringEntry.h"
 #include "NVSIntegerEntry.h"
 
+
 #define NVS_NAMESPACE_NAME    "WILCO"
-#define NVS_KEY_WIFISSID      "wifi_ssid"
-#define NVS_KEY_WIFIPASSWORD  "wifi_password"
-#define NVS_KEY_MQTTSERVER    "mqtt_server"
-#define NVS_KEY_MQTTPORT      "mqtt_port"
-#define NVS_KEY_MQTTUSERNAME  "mqtt_username"
-#define NVS_KEY_MQTTPASSWORD  "mqtt_password"
+
+#define NVS_KEY_WIFISSID       "wifi_ssid"
+#define NVS_LABEL_WIFISSID     "WiFi SSID"
+
+#define NVS_KEY_WIFIPASSWORD   "wifi_password"
+#define NVS_LABEL_WIFIPASSWORD "WiFi Password"
+
+#define NVS_KEY_MQTTSERVER     "mqtt_server"
+#define NVS_LABEL_MQTTSERVER   "MQTT Server"
+
+#define NVS_KEY_MQTTPORT       "mqtt_port"
+#define NVS_LABEL_MQTTPORT     "MQTT Port"
+
+#define NVS_KEY_MQTTUSERNAME   "mqtt_username"
+#define NVS_LABEL_MQTTUSERNAME "MQTT Username"
+
+#define NVS_KEY_MQTTPASSWORD   "mqtt_password"
+#define NVS_LABEL_MQTTPASSWORD "MQTT Password"
+
 
 #define AP_SSID      "WILCO AP"
 #define AP_PASSWORD  NULL
@@ -22,19 +36,21 @@
 
 
 class WILCO {
+public:
 	WILCO() :
 		_namespace(NVSNamespace(NVS_NAMESPACE_NAME)),
-		_settingWiFiSSID(_namespace.stringEntry(NVS_KEY_WIFISSID)),
-		_settingWiFiPassword(_namespace.stringEntry(NVS_KEY_WIFIPASSWORD)),
-		_settingMQTTServer(_namespace.stringEntry(NVS_KEY_MQTTSERVER)),
-		_settingMQTTPort(_namespace.integerEntry(NVS_KEY_MQTTPORT)),
-		_settingMQTTUsername(_namespace.stringEntry(NVS_KEY_MQTTUSERNAME)),
-		_settingMQTTPassword(_namespace.stringEntry(NVS_KEY_MQTTPASSWORD))
+		_settingWiFiSSID(_namespace.stringEntry(NVS_KEY_WIFISSID, NVS_LABEL_WIFISSID)),
+		_settingWiFiPassword(_namespace.stringEntry(NVS_KEY_WIFIPASSWORD, NVS_LABEL_WIFIPASSWORD)),
+		_settingMQTTServer(_namespace.stringEntry(NVS_KEY_MQTTSERVER, NVS_LABEL_MQTTSERVER)),
+		_settingMQTTPort(_namespace.integerEntry(NVS_KEY_MQTTPORT, NVS_LABEL_MQTTPORT)),
+		_settingMQTTUsername(_namespace.stringEntry(NVS_KEY_MQTTUSERNAME, NVS_LABEL_MQTTUSERNAME)),
+		_settingMQTTPassword(_namespace.stringEntry(NVS_KEY_MQTTPASSWORD, NVS_LABEL_MQTTPASSWORD))
 	{};
 	void begin();
 	void loop();
 
 private:
+	IPAddress _ip;
 	NVSNamespace _namespace;
 	NVSStringEntry _settingWiFiSSID;
 	NVSStringEntry _settingWiFiPassword;
@@ -47,9 +63,16 @@ private:
 	void _startClientMode();
 	void _startDNSServer();
 	void _startWebServer();
+	void _startMQTTClient();
+	String _mqttId();
 
-	String _generateFormField(NVSStringEntry *setting, bool isPassword);
+	void _serveSettingsForm();
+	void _handleSettingsForm();
+
+	String _generateFormField(NVSStringEntry *setting, bool isPassword = false);
 	String _generateFormField(NVSIntegerEntry *setting);
+
+	void _handleMQTTMessage(char * topic, byte * payload, unsigned int length);
 };
 
 #endif
